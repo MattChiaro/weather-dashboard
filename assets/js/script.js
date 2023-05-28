7fdedbcb5dc50576c4ab15ea1311395850e1ab28
Matt Chiaro <131219830+MattChiaro@users.noreply.github.com>
2023-05-27 22:57:56 -0500

    button working, both fetchs for coord and weather

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,36 @@
-var weatherApiUrl = 'http://api.openweathermap.org/data/2.5/forecast?id=524901&appid='
-var geoApiUrl = 'http://api.openweathermap.org/geo/1.0/direct?q='
-
+var cityNameEl = document.querySelector('#city-name')
+var qInput = document.querySelector('#q')
+var searchForm = document.querySelector('#city-search')
 var apiKey = 'e15c17ab5737c79cccfc6cb26a3943b2'
 
-var cityNameEl = document.querySelector('#city-name')
 
+var handleSearch = function(event) {
+    event.preventDefault();
+
+    var q = qInput.value.trim();
+
+    
+    var geoApiUrl = `http://api.openweathermap.org/geo/1.0/direct?q=${q}&appid=${apiKey}`
+
+
+    fetch(geoApiUrl)
+        .then(function (response) {
+            return response.json();
+        })
+        .then(function (data) {
+            var lat = data[0].lat;
+            var lon = data[0].lon;
+
+            var weatherApiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${apiKey}`
+
+            fetch(weatherApiUrl)
+            .then(function (response) {
+                return response.json();
+            })
+            .then(function (data) {
+                console.log(data);
+        })
+})
+}
+
+searchForm.addEventListener('submit', handleSearch);
