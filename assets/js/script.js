18d64f3cde99353647c44482df9b8dd3a425080d
Matt Chiaro <131219830+MattChiaro@users.noreply.github.com>
2023-05-27 20:13:54 -0500

    set  basic var in JS

---
+++ assets/js/script.js
@@ -0,0 +1,7 @@
+var weatherApiUrl = 'http://api.openweathermap.org/data/2.5/forecast?id=524901&appid='
+var geoApiUrl = 'http://api.openweathermap.org/geo/1.0/direct?q='
+
+var apiKey = 'e15c17ab5737c79cccfc6cb26a3943b2'
+
+var cityNameEl = document.querySelector('#city-name')
+
