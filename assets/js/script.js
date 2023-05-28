b96072fc70d9dc45edf19cf04129756b05c83b71
Matt Chiaro <131219830+MattChiaro@users.noreply.github.com>
2023-05-27 23:22:09 -0500

    populates main card with current weather data

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,6 +2,10 @@ var cityNameEl = document.querySelector('#city-name')
 var qInput = document.querySelector('#q')
 var searchForm = document.querySelector('#city-search')
 var apiKey = 'e15c17ab5737c79cccfc6cb26a3943b2'
+var currentTempEl = document.querySelector('#current-temp')
+var currentFeelsLikeEl = document.querySelector('#current-feels-like')
+var currentHumidityEl = document.querySelector('#current-humidity')
+var currentDescriptionEl = document.querySelector('#current-description')
 
 
 var handleSearch = function(event) {
@@ -21,7 +25,7 @@ var handleSearch = function(event) {
             var lat = data[0].lat;
             var lon = data[0].lon;
 
-            var weatherApiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${apiKey}`
+            var weatherApiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&units=imperial&appid=${apiKey}`
 
             fetch(weatherApiUrl)
             .then(function (response) {
@@ -29,6 +33,23 @@ var handleSearch = function(event) {
             })
             .then(function (data) {
                 console.log(data);
+                var currentCity = data.name;
+                var currentTemp = data.main.temp;
+                var currentFeelsLike = data.main.feels_like;
+                var currentHumidity = data.main.humidity;
+                var currentDescription = data.weather[0].description.toString();
+
+                
+
+
+
+
+                cityNameEl.textContent = `Current Weather for: ${currentCity}`;
+                currentTempEl.textContent = `Temp: ${currentTemp}*F`;
+                currentFeelsLikeEl.textContent = `Feels Like: ${currentFeelsLike}*F`;
+                currentHumidityEl.textContent = `Humidity: ${currentHumidity}%`;
+                currentDescriptionEl.textContent = currentDescription;
+
         })
 })
 }
