f2bfc20f8163e012877cc8e9592e84a0f0e145ea
Matt Chiaro <131219830+MattChiaro@users.noreply.github.com>
2023-05-29 21:59:49 -0500

    updated all var to ES6 (const/let)

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,21 +1,21 @@
-var cityNameEl = document.querySelector('#city-name')
-var qInput = document.querySelector('#q')
-var searchForm = document.querySelector('#city-search')
-var apiKey = 'e15c17ab5737c79cccfc6cb26a3943b2'
-var currentTempEl = document.querySelector('#current-temp')
-var currentWindSpeedEl = document.querySelector('#current-wind-speed')
-var currentHumidityEl = document.querySelector('#current-humidity')
-var currentIconImageEl = document.querySelector('#current-icon-image')
-var searchHistoryEl = document.querySelector('#search-history')
-var q = '';
+const cityNameEl = document.querySelector('#city-name')
+const qInput = document.querySelector('#q')
+const searchForm = document.querySelector('#city-search')
+const apiKey = 'e15c17ab5737c79cccfc6cb26a3943b2'
+const currentTempEl = document.querySelector('#current-temp')
+const currentWindSpeedEl = document.querySelector('#current-wind-speed')
+const currentHumidityEl = document.querySelector('#current-humidity')
+const currentIconImageEl = document.querySelector('#current-icon-image')
+const searchHistoryEl = document.querySelector('#search-history')
+let q = '';
 
-var searchHistory = JSON.parse(localStorage.getItem('searchHistory')) || [];
+let searchHistory = JSON.parse(localStorage.getItem('searchHistory')) || [];
 
 
 //for every item saved in localStorage, create a link and display it on page.
 function displaySearchHistory() {
 
-    var searchHistoryHeaderEl = document.querySelector('#search-history-header')
+    const searchHistoryHeaderEl = document.querySelector('#search-history-header')
 
 
 
@@ -51,7 +51,7 @@ function search() {
 
 
     generateSearchHistory();
-    var geoApiUrl = `https://api.openweathermap.org/geo/1.0/direct?q=${q}&appid=${apiKey}`
+    const geoApiUrl = `https://api.openweathermap.org/geo/1.0/direct?q=${q}&appid=${apiKey}`
 
 
     //fetch geo api to turn the city name into lat and lon coordinates
@@ -60,11 +60,11 @@ function search() {
             return response.json();
         })
         .then(function (data) {
-            var lat = data[0].lat;
-            var lon = data[0].lon;
-            var state = data[0].state;
+            const lat = data[0].lat;
+            const lon = data[0].lon;
+            const state = data[0].state;
 
-            var weatherApiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&units=imperial&appid=${apiKey}`
+            const weatherApiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&units=imperial&appid=${apiKey}`
 
             //using lat and lon coord, fetch the weather api to get current weather data and display it on the page in main card.
             fetch(weatherApiUrl)
@@ -72,12 +72,12 @@ function search() {
                     return response.json();
                 })
                 .then(function (data) {
-                    var currentCity = data.name;
-                    var currentTemp = Math.round(data.main.temp);
-                    var currentWindSpeed = Math.round(data.wind.speed);
-                    var currentHumidity = data.main.humidity;
-                    var iconCode = data.weather[0].icon
-                    var iconImage = `https://openweathermap.org/img/wn/${iconCode}@2x.png`
+                    const currentCity = data.name;
+                    const currentTemp = Math.round(data.main.temp);
+                    const currentWindSpeed = Math.round(data.wind.speed);
+                    const currentHumidity = data.main.humidity;
+                    const iconCode = data.weather[0].icon
+                    const iconImage = `https://openweathermap.org/img/wn/${iconCode}@2x.png`
 
                     currentIconImageEl.setAttribute('src', iconImage)
                     cityNameEl.textContent = `Current Weather for: ${currentCity}, ${state}`;
@@ -85,7 +85,7 @@ function search() {
                     currentWindSpeedEl.textContent = `Wind Speed: ${currentWindSpeed} MPH`;
                     currentHumidityEl.textContent = `Humidity: ${currentHumidity}%`;
 
-                    var fiveDayApiUrl = `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&units=imperial&appid=${apiKey}`
+                    const fiveDayApiUrl = `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&units=imperial&appid=${apiKey}`
 
                     //use same lat and lon to fetch the 5day/3hour api to get future weather data.
                     fetch(fiveDayApiUrl)
@@ -98,26 +98,26 @@ function search() {
                             document.querySelector('.card-group').innerHTML = '';
 
                             // Dynamically displays  '5 Day Forecast' header
-                            var fiveDayHeaderEl = document.querySelector('#five-day-header');
+                            const fiveDayHeaderEl = document.querySelector('#five-day-header');
 
                             fiveDayHeaderEl.className = '';
                             fiveDayHeaderEl.className = 'd-block';
 
                             //increments by 8, since there are 40 responses for the api call - 8 per day. This pulls 5 different days spaced 24 hours apart. Dynamically creates cards for each day with their respective information.
                             for (let i = 0; i < 40; i += 8) {
-                                var temp = Math.round(data.list[i].main.temp);
-                                var wind = Math.round(data.list[i].wind.speed);
-                                var humidity = data.list[i].main.humidity;
-                                var icon = data.list[i].weather[0].icon;
+                                const temp = Math.round(data.list[i].main.temp);
+                                const wind = Math.round(data.list[i].wind.speed);
+                                const humidity = data.list[i].main.humidity;
+                                const icon = data.list[i].weather[0].icon;
 
                                 //build each individual card
-                                var cardTitleEl = document.createElement('h5');
-                                var cardIcon = document.createElement('img');
-                                var cardTempEl = document.createElement('p');
-                                var cardWindEl = document.createElement('p');
-                                var cardHumidityEl = document.createElement('p');
-                                var cardBodyEl = document.createElement('div');
-                                var cardEl = document.createElement('div');
+                                const cardTitleEl = document.createElement('h5');
+                                const cardIcon = document.createElement('img');
+                                const cardTempEl = document.createElement('p');
+                                const cardWindEl = document.createElement('p');
+                                const cardHumidityEl = document.createElement('p');
+                                const cardBodyEl = document.createElement('div');
+                                const cardEl = document.createElement('div');
 
                                 cardTitleEl.className = 'card-title';
                                 cardIcon.className = 'card-icon';
